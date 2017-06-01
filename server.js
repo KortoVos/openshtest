47226217500c466bbd6cddcdcf5f1326019a4cda
KortoVos <larshofschlaeger@gmail.com>
2017-06-01 17:43:48 +0200

    tt

--- server.js
+++ server.js
@@ -2,12 +2,9 @@
 var express = require('express'),
     app     = express();
     
-Object.assign=require('object-assign')
-
-app.engine('html', require('ejs').renderFile);
 
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
     mongoURL = process.env.OPENSHIFT_MONGODB_DB_URL || process.env.MONGO_URL,
     mongoURLLabel = "";
 
@@ -65,10 +62,10 @@ app.get('/', function (req, res) {
     // Create a document with request IP and current time of request
     col.insert({ip: req.ip, date: Date.now()});
     col.count(function(err, count){
-      res.render('index.html', { pageCountMessage : count, dbInfo: dbDetails });
+      res.send('{ pageCount: will come}');
     });
   } else {
-    res.render('index.html', { pageCountMessage : null});
+    res.send('{ pageCount: soon ;)}');
   }
 });
 
